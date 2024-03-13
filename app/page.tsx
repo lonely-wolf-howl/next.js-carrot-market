d6a7ff4d7c135e723e2b4273a1a4ccc5395a10c4
kme-developer <electruc0095@gmail.com>
2024-03-13 19:51:55 +0900

    feat. 리스트 구현 및 애니메이션, 스켈레톤 적용.

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,53 @@
 export default function Home() {
   return (
-    <main
-      className="bg-gray-100 
-    sm:bg-red-500 md:bg-green-500 lg:bg-blue-500 xl:bg-purple-500 
-      h-screen flex items-center justify-center p-5"
-    >
-      <div
-        className="bg-white w-full shadow-lg p-5 rounded-3xl 
-        max-w-screen-sm flex flex-col md:flex-row gap-2 
-        *:outline-none has-[:invalid]:bg-red-100"
-      >
-        <input
-          type="text"
-          placeholder="enter your email"
-          className="w-full rounded-full h-12 bg-gray-200 pl-5
-          ring ring-transparent focus:ring-green-300 focus:ring-offset-2 
-          transition-shadow placeholder:drop-shadow
-          invalid:focus:ring-red-300 peer"
-          required
-        />
-        <span className="text-red-300 font-medium hidden peer-invalid:block">
-          email is required.
-        </span>
-        <button
-          className="bg-black peer-invalid:bg-red-300 
-        text-white py-2 rounded-full
-          active:scale-90 transition-transform font-medium md:px-10"
+    <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
+      <div className="flew fles-col items-center justify-center space-y-5">
+        <div
+          className="bg-white w-full shadow-lg p-5 rounded-3xl 
+        max-w-screen-sm flex flex-col gap-4"
         >
-          LOGIN
-        </button>
+          {['Person A', 'Person B', 'Person C', ''].map((person, index) => (
+            <div
+              key={index}
+              className="flex items-center gap-5 border-b-2 pb-5 
+            last:pb-0 last:border-0"
+            >
+              <div className="size-10 bg-blue-400 rounded-full" />
+              <span
+                className="text-lg font-medium 
+                empty:w-20 empty:h-2 empty:rounded-full empty:animate-pulse empty:bg-gray-300"
+              >
+                {person}
+              </span>
+              <div
+                className="size-6 bg-red-500 text-white 
+              flex items-center justify-center rounded-full relative"
+              >
+                <span className="z-10">{index}</span>
+                <div
+                  className="size-6 bg-red-500 rounded-full 
+                  absolute animate-ping"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+        <div
+          className="bg-white w-full shadow-lg p-5 rounded-3xl 
+        max-w-screen-sm flex flex-col gap-4"
+        >
+          {['Person A', 'Person B', 'Person C'].map((person, index) => (
+            <div
+              key={index}
+              className="flex items-center gap-5 border-b-2 pb-5 
+            last:pb-0 last:border-0 animate-pulse"
+            >
+              <div className="size-10 bg-blue-400 rounded-full" />
+              <div className="w-40 h-2 rounded-full bg-gray-400" />
+              <div className="w-20 h-2 rounded-full bg-gray-400" />
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
