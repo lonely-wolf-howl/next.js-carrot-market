697672cedaa724b15075a375d92a1f55c2bb1890
kme-developer <electruc0095@gmail.com>
2024-03-13 18:30:00 +0900

    feat. 스택 모디파이어 및 peer 연계 설정.

--- app/page.tsx
+++ app/page.tsx
@@ -11,16 +11,22 @@ export default function Home() {
       >
         <input
           type="text"
-          placeholder="Search something here..."
+          placeholder="enter your email"
           className="w-full rounded-full h-12 bg-gray-200 pl-5
-          outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 
-          transition-shadow placeholder:drop-shadow"
+          outline-none ring ring-transparent focus:ring-green-300 focus:ring-offset-2 
+          transition-shadow placeholder:drop-shadow
+          invalid:focus:ring-red-300 peer"
+          required
         />
+        <span className="text-red-300 font-medium hidden peer-invalid:block">
+          email is required.
+        </span>
         <button
-          className="bg-black text-white py-2 rounded-full
+          className="bg-black peer-invalid:bg-red-300
+          text-white py-2 rounded-full
           active:scale-90 outline-none transition-transform font-medium md:px-10"
         >
-          Search
+          LOGIN
         </button>
       </div>
     </main>
