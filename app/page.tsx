8a62517062d845b5d7fd485c3f383c18fafbc1eb
kme-developer <electruc0095@gmail.com>
2024-03-13 18:43:51 +0900

    feat. 가상 클래스를 통한 자식 조건부 선택 및 단체 적용 기능 구현.

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,14 @@ export default function Home() {
     >
       <div
         className="bg-white w-full shadow-lg p-5 rounded-3xl 
-        max-w-screen-sm flex flex-col md:flex-row gap-2"
+        max-w-screen-sm flex flex-col md:flex-row gap-2 
+        *:outline-none has-[:invalid]:bg-red-100"
       >
         <input
           type="text"
           placeholder="enter your email"
           className="w-full rounded-full h-12 bg-gray-200 pl-5
-          outline-none ring ring-transparent focus:ring-green-300 focus:ring-offset-2 
+          ring ring-transparent focus:ring-green-300 focus:ring-offset-2 
           transition-shadow placeholder:drop-shadow
           invalid:focus:ring-red-300 peer"
           required
@@ -22,9 +23,9 @@ export default function Home() {
           email is required.
         </span>
         <button
-          className="bg-black peer-invalid:bg-red-300
-          text-white py-2 rounded-full
-          active:scale-90 outline-none transition-transform font-medium md:px-10"
+          className="bg-black peer-invalid:bg-red-300 
+        text-white py-2 rounded-full
+          active:scale-90 transition-transform font-medium md:px-10"
         >
           LOGIN
         </button>
