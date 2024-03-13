329aa36637a7865b293d25f84d62d40262d4e218
kme-developer <electruc0095@gmail.com>
2024-03-13 18:10:49 +0900

    feat. 화면 크기에 따른 조건부 반응형 디자인 구현.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
+    <main
+      className="bg-gray-100 
+    sm:bg-red-500 md:bg-green-500 lg:bg-blue-500 xl:bg-purple-500 
+      h-screen flex items-center justify-center p-5"
+    >
       <div
         className="bg-white w-full shadow-lg p-5 rounded-3xl 
-      max-w-screen-sm flex flex-col gap-2"
+        max-w-screen-sm flex flex-col md:flex-row gap-2"
       >
         <input
           type="text"
@@ -14,7 +18,7 @@ export default function Home() {
         />
         <button
           className="bg-black text-white py-2 rounded-full
-         active:scale-90 outline-none transition-transform font-medium"
+          active:scale-90 outline-none transition-transform font-medium md:px-10"
         >
           Search
         </button>
