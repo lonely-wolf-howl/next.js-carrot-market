0bcb4fd602826302e8aa8d0df9b6dde987f9d890
kme-developer <electruc0095@gmail.com>
2024-03-14 16:56:05 +0900

    feat. 서버 액션을 활용한 데이터 폼 패칭 구현. 또한 펜딩 상태 조절 예시.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,15 @@
-'use client';
-
 import FormInput from '@/components/form-input';
 import FormButton from '@/components/form-button';
 import SocialLogin from '@/components/social-login';
 
 export default function Login() {
-  const handleOnClick = async () => {
-    const response = await fetch('api/users', {
-      method: 'POST',
-      body: JSON.stringify({
-        username: 'username',
-        password: 'password',
-      }),
+  const handleForm = async (formData: FormData) => {
+    'use server';
+    console.log(formData.get('email'), formData.get('password'));
+    await new Promise((resolve) => {
+      setTimeout(resolve, 3000);
     });
-    console.log(await response.json());
+    console.log('login success!');
   };
 
   return (
@@ -22,23 +18,23 @@ export default function Login() {
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">아래 빈칸을 채워 주세요.</h2>
       </div>
-      <form className="flex flex-col gap-3">
+      <form action={handleForm} className="flex flex-col gap-3">
         <FormInput
+          name="email"
           type="email"
           placeholder="사용자 계정 (이메일)"
           required
           errors={[]}
         />
         <FormInput
+          name="password"
           type="password"
           placeholder="비밀번호"
           required
           errors={[]}
         />
+        <FormButton text="로그인" />
       </form>
-      <span onClick={handleOnClick}>
-        <FormButton loading={false} text="로그인" />
-      </span>
       <SocialLogin />
     </div>
   );
